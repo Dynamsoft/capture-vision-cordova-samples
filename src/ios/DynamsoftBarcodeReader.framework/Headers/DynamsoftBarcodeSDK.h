49a8f9acbcd58e3f54a843dadc15de71aedc4cee
zyunique <335454895@qq.com>
2019-11-22 15:44:27 +0800

    modify

---
+++ src/ios/DynamsoftBarcodeReader.framework/Headers/DynamsoftBarcodeSDK.h
@@ -0,0 +1,19 @@
+//
+//  DynamsoftBarcodeSDK.h
+//  DynamsoftBarcodeSDK
+//
+//  Created by Dynamsoft on 2018/6/7.
+//  Copyright © 2018 Dynamsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamsoftBarcodeSDK.
+FOUNDATION_EXPORT double DynamsoftBarcodeSDKVersionNumber;
+
+//! Project version string for DynamsoftBarcodeSDK.
+FOUNDATION_EXPORT const unsigned char DynamsoftBarcodeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamsoftBarcodeSDK/PublicHeader.h>
+
+#import <DynamsoftBarcodeReader/DynamsoftBarcodeReader.h>
