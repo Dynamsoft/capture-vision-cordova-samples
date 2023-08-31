eb5bd9140a6a56daba7d1b559298df065a1d4524
Dynamsoft-Henry <henry@dynamsoft.com>
2023-08-31 09:13:07 +0800

    Update startup.js

--- BarcodeReaderSimpleSample/template_src/www/js/startup.js
+++ BarcodeReaderSimpleSample/template_src/www/js/startup.js
@@ -5,6 +5,9 @@ async function onDeviceReady() {
     document.getElementById('deviceready').classList.add('ready');
 
     if (!window.localStorage.initLicense) { 
+        // Initialize license for the Barcode Decoding module of Dynamsoft Capture Vision.
+	    // The license string here is a time-limited trial license. Note that network connection is required for this license to work.
+	    // You can also request an extension for your trial license in the customer portal: https://www.dynamsoft.com/customer/license/trialLicense?product=dbr&utm_source=installer&package=cordova
         try {
             await Dynamsoft.DCVBarcodeReader.initLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9");
         } catch (e) {
